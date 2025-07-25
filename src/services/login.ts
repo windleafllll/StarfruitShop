20bda7fc9f9ec7f0a9cdbe7696bf5260b3f52099
Windleafllll <1550347257@qq.com>
2025-07-25 22:29:45 +0800

    用户登录功能的实现

---
+++ src/services/login.ts
@@ -0,0 +1,26 @@
+import type { LoginResult } from '@/types/member'
+import { http } from '@/utils/http'
+
+type LoginParams = {
+  code: string
+  encryptedData: string
+  iv: string
+}
+
+export const postLoginWxAPI = (data: LoginParams) => {
+  return http<LoginResult>({
+    method: 'POST',
+    url: '/login/wxMin',
+    data,
+  })
+}
+
+export const postLoginWxMinSimpleAPI = (phoneNumber: string) => {
+  return http<LoginResult>({
+    method: 'POST',
+    url: '/login/wxMin/simple',
+    data: {
+      phoneNumber,
+    },
+  })
+}
